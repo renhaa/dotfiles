daf5f3c5f14cff070f00c99fdd0c736c697a8221
René Haas <renha@itu.dk>
2020-10-13 21:34:31 +0200

    upd

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,9 +4,12 @@ sudo apt upgrade
 sudo apt autoremove
 sudo apt autoclean
 
+sudo apt install libcurl4-openssl-dev libssl-dev pkg-config
 sudo apt install curl python3-pip neofetch pavucontrol
 sudo apt install texlive texlive-bibtex-extra texlive-science texlive-extra-utils texlive-latex-extra texlive-fonts-extra
 pip3 install matplotlib sklearn numpy tensorflow jupyterlab
 
 sh install_vscode.sh
 
+# Install Pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
--- update.sh
+++ update.sh
@@ -2,4 +2,8 @@ cp -r ~/dotfiles/i3 .config/
 
 i3-msg restart
 grep -qxF 'source ~/dotfiles/aliases' ~/.bashrc || echo 'source ~/dotfiles/aliases' >> ~/.bashrc
+
 cp ~/dotfiles/.vimrc ~/
+cp ~/dotfiles/.bashrc ~/
+
+
